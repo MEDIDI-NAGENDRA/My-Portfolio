d3f364536961d9c41465f47fc0a457101c2cf6a3
MEDIDI-NAGENDRA <nagendramedidi6@gmail.com>
2025-06-23 22:46:36 +0530

    MPF-1

---
+++ Portfolio-main/src/data/UserData.jsx
@@ -0,0 +1,38 @@
+export const UserData = {
+  name: "Nagendra Medidi",
+  about : "I'm a self-taught full stack developer skilled in building scalable web applications using Spring Boot, React.js, and SQL. I create responsive, user-friendly interfaces and robust back-end systems, with strong version control practices using Git. My solutions consistently enhance performance and user experience across platforms.",
+  resumeUrl : "https://drive.google.com/file/d/1kR0qObESajTTpyFymXBFUAHWhjY-Vd6M/view?usp=sharing",
+  FooterLink : "https://www.linkedin.com/in/nagendra-medidi-bab0a3255/",
+  socialMedia: [
+    {
+      socialMediaName: "github",
+      url: "https://github.com/MEDIDI-NAGENDRA",
+      icon: "AiFillGithub",
+    },
+    {
+      socialMediaName: "linkedin",
+      url: "https://www.linkedin.com/in/nagendra-medidi-bab0a3255/",
+      icon: "FaLinkedinIn",
+    },
+    {
+      socialMediaName: "twitter",
+      url: "#",
+      icon: "AiOutlineTwitter",
+    },
+    {
+      socialMediaName: "instagram",
+      url: "#",
+      icon: "AiFillInstagram",
+    },
+  ],
+  typewriterOptions: {
+    strings: [
+      "Full Stack Developer",
+      "Spring Boot Developer",
+      
+    ],
+    autoStart: true,
+    loop: true,
+    deleteSpeed: 30,
+  },
+};
